6a6b59cc618c9b01184e34be0553622c6be16feb
James MacIsaac <james@jmproj.com>
2018-02-23 12:57:16 -0700

    Added the basic project that will hold the mapsindoors tutorial code

---
+++ MapsIndoorsTutorial/MapsIndoorsTutorial/MapsIndoorsTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapsIndoorsTutorial
+//
+//  Created by James MacIsaac on 2018-02-23.
+//  Copyright © 2018 James MacIsaac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
